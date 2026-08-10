04ccccabd035b89406a752e529169155b411cd36
cristolution <cyaghjian2004@outlook.com>
2026-08-10 23:32:47 +0300

    chor: targeted to local host

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://mgf.vortex-tech.tech',
+        target: 'http://localhost:8000',
         changeOrigin: true,
       },
     },
